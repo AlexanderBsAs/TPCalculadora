97a6c256238f93c36e3e1c0e29c4703447c894f7
Alexander <alexitorjlanus@gmail.com>
2023-09-24 22:56:35 -0300

    borro registros3

--- modulos/data/registros3.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const multiplicar=require("./multiplicar.js")
-const sumar=require("./suma.js")
-const division=require("./division.js")
-const registro =[ {
-    operacion: sumar,
-    resultado: sumar
-   }]
